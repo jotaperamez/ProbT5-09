5de5511724071387185e437021115b94f37f4461
Juan Pedro Ramos Gómez <jrg065@inlumine.ual.es>
2016-05-28 12:42:07 +0200

    main scene

---
+++ MatsMania/Assets/scripts/FallingNumbers.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingNumbers : MonoBehaviour {
+
+    public int numero;
+
+	// Use this for initialization
+	void Start () {
+      
+    }
+	
+
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(new Vector2(0, -10) * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(this);
+       }
+
+    }
+}
