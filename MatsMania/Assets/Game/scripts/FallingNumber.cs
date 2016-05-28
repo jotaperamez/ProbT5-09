0bc532534e1f5c261c490f5612013b418b798c52
Juan Pedro Ramos Gómez <jrg065@inlumine.ual.es>
2016-05-28 16:54:03 +0200

    commiting

---
+++ MatsMania/Assets/Game/scripts/FallingNumber.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingNumber : MonoBehaviour {
+
+    public int value;
+
+	// Use this for initialization
+	void Start () {
+
+        value = Random.Range(0, 100);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- MatsMania/Assets/Game/scripts/FallingNumbersMaganer.cs
+++ MatsMania/Assets/Game/scripts/FallingNumbersMaganer.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class FallingNumbersMaganer : MonoBehaviour {
 
+
+    public GameObject ficha;
+    public Sprite sprite;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+       
+            
+                
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        ficha.AddComponent<Rigidbody2D>();
+        ficha.transform.position = new Vector2(updateX(),10);
+    }
+
+    public int updateX()
+    {
+        return Random.Range(-10, 10);
+    }
 }
